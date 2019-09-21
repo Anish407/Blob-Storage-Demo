46a4d425c835eaa6db3357bec6b35fe9dc4d7921
Anish Aravind <anish.arvind407@gmail.com>
2019-09-21 22:04:44 +0530

    With Token AUthentication

---
+++ JwtSample/JwtSample/Controllers/JwtController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtSample.Controllers
+{
+    [Route("api/jwt")]
+    [ApiController]
+    public class JwtController:ControllerBase
+    {
+        private IConfiguration _config;
+
+        public JwtController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            //Validate the User Credentials  
+            //Demo Purpose, I have Passed HardCoded User Information  
+            if (login.Name == "Jignesh")
+            {
+                user = new UserModel { Name = "Jignesh Trivedi", Password = "test.btest@gmail.com" };
+            }
+            return user;
+        }
+
+    }
+
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ JwtSample/JwtSample/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtSample.Models
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Student> MyProperty { get; set; }
+    }
+}
